782a0c50c2b338205c9483d4ddc325568db818c4
David Young-Chan Kay <davidykay@gmail.com>
2015-03-09 19:33:04 -0400

    added push code to AppDelegate.

--- HelloPushXamarin.Forms/iOS/AppDelegate.cs
+++ HelloPushXamarin.Forms/iOS/AppDelegate.cs
@@ -13,11 +13,41 @@ namespace HelloPushXamarin.Forms.iOS
     public override bool FinishedLaunching (UIApplication app, NSDictionary options)
     {
       global::Xamarin.Forms.Forms.Init ();
+      
+      Console.WriteLine ("Finished Launching");
+      Console.WriteLine ("bundle ID: " + NSBundle.MainBundle.BundleIdentifier);
+
+      app.RegisterForRemoteNotifications();
 
       LoadApplication (new App ());
 
       return base.FinishedLaunching (app, options);
     }
+
+    public override void ReceivedRemoteNotification(UIApplication application, NSDictionary userInfo)
+    {
+      Console.WriteLine ("ReceivedRemoteNotification (foreground)");
+
+    }
+
+    public override void DidReceiveRemoteNotification (UIApplication application, NSDictionary userInfo, Action<UIBackgroundFetchResult> completionHandler) {
+      Console.WriteLine ("DidReceiveRemoteNotification (background)");
+    }
+
+    public override void RegisteredForRemoteNotifications (UIApplication application, NSData deviceToken) {
+      Console.WriteLine ("RegisteredForRemoteNotifications: " + deviceToken.ToString());
+
+    }
+
+    public override void DidRegisterUserNotificationSettings (UIApplication application, UIUserNotificationSettings notificationSettings) {
+      Console.WriteLine ("DidRegisterUserNotificationSettings: " + notificationSettings);
+    }
+
+    public override void FailedToRegisterForRemoteNotifications (UIApplication application, NSError error) {
+      Console.WriteLine ("FailedToRegisterForRemoteNotifications" + error);
+
+    }
+
   }
 }
 
